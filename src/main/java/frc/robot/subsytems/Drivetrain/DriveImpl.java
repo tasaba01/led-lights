3ce529462bdf88891b5825ce4c63c9cd92b778eb
Sebastian Vega <sebasvega4604@gmail.com>
2022-01-23 18:39:01 -0500

    Closes #10

--- src/main/java/frc/robot/subsytems/Drivetrain/DriveImpl.java
+++ src/main/java/frc/robot/subsytems/Drivetrain/DriveImpl.java
@@ -7,7 +7,6 @@ import ca.team3161.lib.robot.subsystem.RepeatingPooledSubsystem;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpiutil.math.Pair;
-import frc.robot.RobotMap;
 
 public class DriveImpl extends RepeatingPooledSubsystem implements Drive {
     
@@ -19,13 +18,13 @@ public class DriveImpl extends RepeatingPooledSubsystem implements Drive {
     Encoder leftEncoder;
     Encoder rightEncoder;
 
-    public DriveImpl(SpeedControllerGroup leftSide, SpeedControllerGroup rightSide) {
+    public DriveImpl(SpeedControllerGroup leftSide, SpeedControllerGroup rightSide, Encoder leftEncoder, Encoder rightEncoder) {
         super(20, TimeUnit.MILLISECONDS);
         this.leftSide = leftSide;
         this.rightSide = rightSide;
         this.drivetrain = new DifferentialDrive(leftSide, rightSide);
-        this.leftEncoder = new Encoder(RobotMap.LEFT_ENCODER_PORTS[0], RobotMap.LEFT_ENCODER_PORTS[1], false, Encoder.EncodingType.k2X);
-        this.rightEncoder = new Encoder(RobotMap.RIGHT_ENCODER_PORTS[0], RobotMap.RIGHT_ENCODER_PORTS[1], false, Encoder.EncodingType.k2X);
+        this.leftEncoder = leftEncoder; 
+        this.rightEncoder = rightEncoder;
     }
 
     @Override
@@ -63,7 +62,7 @@ public class DriveImpl extends RepeatingPooledSubsystem implements Drive {
 
     @Override
     public Pair<Double, Double> distanceDriven() {
-        // TODO Auto-generated method stub
-        return Pair.of(0.0, 0.0);
+        // return distance of either encoder
+        return Pair.of(this.leftEncoder.getDistance(), this.rightEncoder.getDistance());
     }
 }
