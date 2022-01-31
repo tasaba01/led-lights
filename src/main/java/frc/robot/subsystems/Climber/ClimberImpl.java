7a0b97e7ba61becc28bdcc724f826cd8c1996be1
duui <67356442+Duui3111@users.noreply.github.com>
2022-01-31 15:36:58 -0500

    Update ClimberImpl.java

--- src/main/java/frc/robot/subsystems/Climber/ClimberImpl.java
+++ src/main/java/frc/robot/subsystems/Climber/ClimberImpl.java
@@ -1,21 +1,56 @@
 package frc.robot.subsystems.Climber;
 
-// import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-// import edu.wpi.first.wpilibj.DoubleSolenoid;
-// import edu.wpi.first.wpilibj.Timer;
-// import frc.robot.RobotMap;
+import java.lang.Thread;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import frc.robot.RobotMap;
 
 public class ClimberImpl implements Climber {
-    @Override
-    public void extendOuterClimber(){}
+        
+    WPI_TalonSRX lifterMotorController;
+    DoubleSolenoid climberSolenoid;
+
+    public ClimberImpl() {
+        this.lifterMotorController = new WPI_TalonSRX(RobotMap.LIFTER_TALON_PORT);
+        this.climberSolenoid = new DoubleSolenoid(RobotMap.CLIMBER_SOLENOID_CHANNELS[0], RobotMap.CLIMBER_SOLENOID_CHANNELS[1]);
+    }
 
     @Override
-    public void retractOuterClimber(){}
+    public void extendOuterClimber() {
+        try {
+            this.lifterMotorController.set(1); // test value
+            Thread.sleep(5000);
+            this.lifterMotorController.set(0);
+        } 
+        catch(Exception e) { 
+            System.out.println(e.getMessage());
+        }
+    }
 
     @Override
-    public void angleOuter(double angle){}
+    public void retractOuterClimber() {
+        try {
+            this.lifterMotorController.set(1); // test value
+            Thread.sleep(5000);
+            this.lifterMotorController.set(0);
+        } 
+        catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
     @Override
-    public void attachInner(){}
+    public void angleOuter(double angle) {}
+
+    @Override
+    public void attachInner() {
+        try {
+            Thread.sleep(650);
+        } 
+        catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
 }
