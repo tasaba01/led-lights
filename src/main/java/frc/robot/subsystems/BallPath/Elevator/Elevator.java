d2c1da1b7373c491b783e7586605a24ad5b893ed
Andrew Azores <me@andrewazor.es>
2022-03-02 20:29:41 -0500

    subsystems state machines

--- src/main/java/frc/robot/subsystems/BallPath/Elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/BallPath/Elevator/Elevator.java
@@ -3,9 +3,14 @@ package frc.robot.subsystems.BallPath.Elevator;
 import ca.team3161.lib.robot.subsystem.Subsystem;
 
 public interface Elevator extends Subsystem{
-    // Checks if balls are primed and where they are primed; ex. one at bottom of elevator and one in left intake
-    void start();
-    void reverse();
-    void stop();
-    // can be used to stop a ball from going up the elevator in the event that we cannot shoot the ball
+    void setAction(ElevatorAction action);
+    boolean ballPrimed();
+
+    enum ElevatorAction {
+        NONE,
+        PRIME,
+        REJECT,
+        FEED,
+        ;
+    }
 }
--- src/main/java/frc/robot/subsystems/BallPath/Intake/Intake.java
+++ src/main/java/frc/robot/subsystems/BallPath/Intake/Intake.java
@@ -2,14 +2,15 @@ package frc.robot.subsystems.BallPath.Intake;
 
 import ca.team3161.lib.robot.subsystem.Subsystem;
 
-
 public interface Intake extends Subsystem{
-    // checks colour of ball
-    void start();
-    void reverse();
-    void stop();
+    void setAction(IntakeAction action);
+    boolean ballPrimed();
 
-    boolean checkIntake();
-    boolean checkColour();
-    // Assuming current design of 2 to 3 rollers in the robot.
+    enum IntakeAction {
+        NONE,
+        PRIME,
+        REJECT,
+        FEED,
+        ;
+    }
 }
